5bef58a6422403947f532d5310f8b5ee02d31754
ksalcce <62475617+ksalcce@users.noreply.github.com>
2020-07-07 10:26:10 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,121 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+  {
+      question: 'What is Javascript ?',
+      choice1: 'JavaScript is to program the behavior of web pages',
+      choice2: 'JavaScript is to to specify the layout of web pages',
+      choice3: 'JavaScript is to to define the content of web pages',
+      choice4: 'JavaScript is something cool to run',
+      answer: 1,
+  },
+    {
+        question: 'Inside which HTML element do we put the JavaScript??',
+        choice1: '<script>',
+        choice2: '<javascript>',
+        choice3: '<js>',
+        choice4: '<scripting>',
+        answer: 1,
+    },
+    {
+        question:
+            "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script name='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<script file='xxx.js'>",
+        answer: 3,
+    },
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    },
+    {
+        question: " Which of the following function of String object combines the text of two strings and returns a new string?",
+        choice1: "add()",
+        choice2: "merge()",
+        choice3: " concat()",
+        choice4: "append()",
+        answer: 3,
+    },
+];
+
+// constants
+
+const CORRECT_BONUS = 20;
+const MAX_QUESTIONS = 5;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
